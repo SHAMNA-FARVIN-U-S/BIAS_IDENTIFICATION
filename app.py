ace3d567f1acdef3747f7933d9b9ccbc71f75951
SHAMNA-FARVIN-U-S <127471999+SHAMNA-FARVIN-U-S@users.noreply.github.com>
2024-01-07 18:40:41 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,54 @@
+import streamlit as st
+import torch
+from transformers import BertTokenizer, BertForSequenceClassification
+
+# Load your BERT model
+
+tokenizer = BertTokenizer.from_pretrained("bert-base-uncased")
+model_bert = torch.load("bert_model.pth",map_location=torch.device('cpu'))
+model_bert.eval()
+
+# Load your label mapping
+label_mapping = {
+    0: "gender_bias",
+    1: "religion_bias",
+    2: "country_bias",
+    3: "non_bias",
+}
+
+# Streamlit app
+st.title("Bias Prediction App")
+
+Type = st.radio(
+    "Select input type",
+    ["Enter a statement", "Select predefined statement"])
+if Type == 'Enter a statement':
+    text_to_classify_bert = st.text_area("Enter text for bias prediction", "")
+else:
+    text_to_classify_bert = st.selectbox(
+    'Select a statement from below',
+    ("My belief is that America has always been superior compared to others.",
+      "The female nurse assisted the male doctor in the surgery",
+        "He's an atheist, so he does not have any morals",
+        "Our county boasts diverse landscapes and a vibrant community, offering opportunities and experiences that appeal to various interests and preferences."),
+    index=None,
+    placeholder="Select statement...",)
+
+st.write('Your input:', text_to_classify_bert)
+
+# Tokenize input text and make prediction
+if st.button("Predict Bias"):
+    if text_to_classify_bert:
+        tokenizer = BertTokenizer.from_pretrained("bert-base-uncased")
+        input_ids_bert = tokenizer(text_to_classify_bert, padding='max_length', truncation=True, max_length=128, return_tensors='pt')['input_ids']
+        attention_mask_bert = tokenizer(text_to_classify_bert, padding='max_length', truncation=True, max_length=128, return_tensors='pt')['attention_mask']
+
+        with torch.no_grad():
+            output_bert = model_bert(input_ids_bert, attention_mask=attention_mask_bert)
+
+        predicted_label_bert = torch.argmax(output_bert.logits, dim=1).item()
+        predicted_bias_bert = label_mapping[predicted_label_bert]
+
+        st.success(f"Predicted Bias: {predicted_bias_bert}")
+    else:
+        st.warning("Please enter some text for prediction.")
---
+++ bert_inference.py
@@ -0,0 +1,51 @@
+import torch
+import torch.nn as nn
+import torch.optim as optim
+from torch.utils.data import DataLoader, TensorDataset
+from sklearn.model_selection import train_test_split
+import pandas as pd
+from transformers import BertTokenizer, BertForSequenceClassification, AdamW
+from sklearn.metrics import accuracy_score
+import os
+
+file_path = os.path.abspath('bert_model.pth')
+
+
+tokenizer = BertTokenizer.from_pretrained("bert-base-uncased")
+model_bert = torch.load("bert_model.pth",map_location=torch.device('cpu'))
+# Test a sample text
+text_to_classify_bert = "women are good at Multitasking"
+input_ids_bert = tokenizer(text_to_classify_bert, padding='max_length', truncation=True, max_length=128, return_tensors='pt')['input_ids']
+attention_mask_bert = tokenizer(text_to_classify_bert, padding='max_length', truncation=True, max_length=128, return_tensors='pt')['attention_mask']
+
+with torch.no_grad():
+    model_bert.eval()
+    output_bert = model_bert(input_ids_bert, attention_mask=attention_mask_bert)
+# Determine the predicted label for BERT model
+predicted_label_bert = torch.argmax(output_bert.logits, dim=1).item()
+
+dataset_path = "BIAS_DATASET.csv"
+df = pd.read_csv(dataset_path)
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+
+texts = df['TEXT'].tolist()
+text_labels = df['LABEL'].tolist()
+
+# Map the predicted label back to the label name using the label_mapping
+label_mapping = {
+    "gender_bias": 0,
+    "religion_bias": 1,
+    "country_bias": 2,
+    "non_bias": 3,
+}
+
+# Encode text labels as integers using the label mapping
+labels = [label_mapping[label] for label in text_labels]
+for label, value in label_mapping.items():
+    if value == predicted_label_bert:
+        predicted_bias_bert = label
+        break
+
+# Print the prediction for BERT model
+print(f"{text_to_classify_bert}: {predicted_bias_bert}")
